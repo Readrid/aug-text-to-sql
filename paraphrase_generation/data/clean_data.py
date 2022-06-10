7cf2690b2209d5d7a2b66d551dbc63062c35ac93
Nikita Usoltsev <57728940+usoltsev37@users.noreply.github.com>
2022-06-11 01:13:55 +0300

    Add full dataset

--- paraphrase_generation/data/clean_data.py
+++ paraphrase_generation/data/clean_data.py
@@ -6,15 +6,25 @@ PARAPHRASES = "paraphrases"
 SENTENCE = "sentence"
 TAG = "tag"
 DATA = "data"
+MIN_СNT_PARAPHRASES = 1
 
 
 def clean_imdb(data, file_name):
+    """
+    Function for cleaning imdb dataset:
+    1. Discard repetitions
+    2. Discard sentences with no phrases
+    3. Discard phrases from one word
+    4. Lead to one register
+    5. Bring to a common appearance
+    """
+
     for element in data[DATA]:
         dict = {}
         if "paraphases" not in element:
             continue
         paraphrases = list(set(element["paraphases"]))
-        if len(paraphrases) < 2:
+        if len(paraphrases) < MIN_СNT_PARAPHRASES:
             continue
 
         sentence = element[SENTENCE].lower()
@@ -31,6 +41,15 @@ def clean_imdb(data, file_name):
 
 
 def clean_others(data, file_name):
+    """
+    Function for cleaning others dataset:
+    1. Discard repetitions
+    2. Discard sentences with no phrases
+    3. Discard phrases from one word
+    4. Lead to one register
+    5. Bring to a common appearance
+    """
+
     for element in data:
         dict = {}
         element = list(set(element))
@@ -41,7 +60,7 @@ def clean_others(data, file_name):
 
         paraphrases = element
 
-        if len(paraphrases) < 2:
+        if len(paraphrases) < MIN_СNT_PARAPHRASES:
             continue
 
         sentence = sentence.lower()
@@ -57,18 +76,25 @@ if __name__ == "__main__":
     common_path = "./dirty/"
 
     file_names = ["imdb.json", "atis.json", "geography.json", "scholar.json", "spider.json"]
+    size_logger = ""
 
     for file_name in file_names:
         with open(common_path + file_name) as jsonFile:
             data = json.load(jsonFile)
         jsonFile.close()
 
+        old_size = len(result_dict[DATA])
+
         if file_name == "imdb.json":
             clean_imdb(data, file_name)
         else:
             clean_others(data, file_name)
 
-    print(f"\tDataset size is {len(result_dict[DATA])}")
+        size_logger += f"{file_name} size is {len(result_dict[DATA]) - old_size}\n"
+
+    size_logger += f"\tFull dataset size is {len(result_dict[DATA])}\n"
+    with open("./clean/size.txt", "w") as outfile:
+        outfile.write(size_logger)
 
     json_object = json.dumps(result_dict, indent=2)
     with open("./clean/dataset.json", "w") as outfile:
